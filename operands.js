4c354eb48c9498b6c058d3f84805ac796c3c7c3c
lucifer555-ux <65466352+lucifer555-ux@users.noreply.github.com>
2024-11-06 13:18:20 +0530

    Completed Chapter 2

---
+++ operands.js
@@ -0,0 +1,10 @@
+// Exponentiation **
+// The exponentiation operator a ** b raises a to the power of b.
+
+// In school maths, we write that as ab.
+
+// For instance:
+
+alert(2 ** 2); // 2² = 4
+alert(2 ** 3); // 2³ = 8
+alert(2 ** 4); // 2⁴ = 16
---
+++ typeconversions.js
@@ -0,0 +1,7 @@
+console.log("Type Conversions");
+
+let value = true;
+alert(typeof value);
+
+value = String(value);
+alert(typeof value);
