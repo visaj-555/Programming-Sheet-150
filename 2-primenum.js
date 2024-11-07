6687959eb482cb03c7ea0f2c32bf135714c27088
lucifer555-ux <65466352+lucifer555-ux@users.noreply.github.com>
2024-11-07 22:08:05 +0530

    Solved 3 Problems

---
+++ 2-primenum.js
@@ -0,0 +1,11 @@
+// Checking for Prime Number
+
+number = 7;
+
+if (number == 2) {
+  console.log("not prime");
+}
+
+for (let i = 2; i < Math.sqrt(number); i++) {
+  console.log("Prime");
+}
---
+++ 3-leapyear.js
@@ -0,0 +1,7 @@
+year = 2020;
+
+if (year % 4 == 0 && year % 100 != 0) {
+  console.log("Leap Year");
+} else {
+  console.log("Not leap year");
+}
